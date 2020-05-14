455266f7f72b8a74da7ebec4be16ddf8b8d7c39c
Camilo piza <camiloandrespiza@gmail.com>
2020-05-13 19:37:14 -0500

    Se agrega agregan dependecias para cordova y se implementa funcionalidad streaming audio

--- Poliradio/src/app/tab2/tab2.module.ts
+++ Poliradio/src/app/tab2/tab2.module.ts
@@ -5,9 +5,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     IonicModule,
     CommonModule,
     FormsModule,

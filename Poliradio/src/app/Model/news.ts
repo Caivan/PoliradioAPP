cc5b3c25535ad6bb2d41b4c5413c064a531ad22e
Juan Pablo Salamanca <jpsalamanca99@gmail.com>
2020-05-16 17:26:44 -0500

    Adelanto de las noticias

---
+++ Poliradio/src/app/Model/news.ts
@@ -0,0 +1,9 @@
+export class news{
+    title : string;
+    content : string;
+
+    constructor (title, content){
+        this.content = content;
+        this.title = title;
+    }
+}
--- Poliradio/src/app/tab1/tab1.page.ts
+++ Poliradio/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
+import { WordPressConnectionService } from "../word-press-connection.service";
+import { CompileShallowModuleMetadata } from '@angular/compiler';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  news;
+  index = 1;
+  maxPage = 1;
+
+  constructor(private wpConnection: WordPressConnectionService) {
+    this.getNews();
+  }
+
+  async getNews() {
+    this.wpConnection.getNewsFromPage(this.index).subscribe(resp => {
+      const keys = resp.headers.keys();
+      let headers = keys.map(key =>
+        `${key}: ${resp.headers.get(key)}`);
+      this.news = resp.body;
+      this.maxPage = Number(headers[5].substring(headers[5].length - 3, headers[5].length));
+    });
+  }
+
+  public nextPage() {
+    if(this.index < this.maxPage){
+      console.log(this.index);
+      this.index++;
+      this.getNews();
+    }
+  }
+
+  public previousPage() {
+    if (this.index > 1) {
+      console.log(this.index);
+      this.index = Math.max(1, this.index--);
+      this.getNews();
+    }
+  }
 
 }

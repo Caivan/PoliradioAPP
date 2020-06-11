156668c4cbf02b2832d87614c692ec271d27a381
Juan Pablo Salamanca <jpsalamanca99@gmail.com>
2020-06-11 14:25:41 -0500

    Solucion de la recarga infinita

--- Poliradio/src/app/pipes/keep-html.pipe.ts
+++ Poliradio/src/app/pipes/keep-html.pipe.ts
@@ -1,11 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-@Pipe({ name: 'keepHtml', pure: false })
+@Pipe({ name: 'keepHtml' })
 export class EscapeHtmlPipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {
-  }
-
+  constructor(private sanitizer: DomSanitizer) {}
+  
   transform(content) {
     return this.sanitizer.bypassSecurityTrustHtml(content);
   }
